f76525b668b6c88119cc16b0d766f387fc62a738
tianlin <tianlin.li@adspower.net>
2023-08-08 17:01:16 +0800

    配置文件

--- api/v1/controller/MysqlController.go
+++ api/v1/controller/MysqlController.go
@@ -1,11 +1,15 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type User struct {
@@ -18,24 +22,58 @@ func (User) TableName() string {
 }
 
 func MysqlController(c *gin.Context) {
-	mysqlDsn := "root:root@tcp(127.0.0.1:3306)/dev?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(mysqlDsn), &gorm.Config{})
 
-	if err != nil {
-		panic("failed to connect database")
-	}
 	id := c.Query("id")
 	var user User
 
-	// 事务
-	tx := db.Begin()
-
-	tx.First(&user, id)
-	tx.Update("name", "test2").Where("id = ?", id).Commit()
+	go updateOne(id, &user)
+	go readOne(id, &user)
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 		"data":    user,
 		"params":  id,
 	})
+}
+
+func updateOne(id string, user *User) {
+	mysqlDsn := viper.GetString("mysql.dsn")
+	db, err := gorm.Open(mysql.Open(mysqlDsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	tx := db.Begin()
+	tx.Exec("SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED")
+	tx.First(&user, id)
+	user.Name = "先请求"
+	tx.Save(&user)
+	time.Sleep(10 * time.Second)
+	tx.Rollback()
+}
+
+func readOne(id string, user *User) {
+	mysqlDsn := viper.GetString("mysql.dsn")
+	db, err := gorm.Open(mysql.Open(mysqlDsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// 事务级别是读未提交
+	tx := db.Begin()
+	tx.Exec("SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED")
+
+	time.Sleep(5 * time.Second)
+	tx.First(&user, id)
+
+	fmt.Println(user)
+
+	time.Sleep(5 * time.Second)
+	tx.Commit()
 }
--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"app_go/api/v1/controller"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigFile("./config/config.yaml") // 设置配置文件路径
+	err := viper.ReadInConfig()                 // 读取配置文件
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	port := viper.GetString("server.port") // 获取端口配置
+
 	r := gin.Default()
 
 	r.GET("/ping", controller.MysqlController)
-	r.Run()
+	r.Run(":" + port)
 }
